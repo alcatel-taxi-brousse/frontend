94a5decb813b63e163f2b3b6e6a02628ecdb9493
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-05-04 14:24:27 +0200

    Login button action

--- app/src/main/java/com/alcatelcnamisi1/taxibrousse/LoginActivity.kt
+++ app/src/main/java/com/alcatelcnamisi1/taxibrousse/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.alcatelcnamisi1.taxibrousse
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private var editTextLogin: EditText? = null
+    private var editTextPassword: EditText? = null
+    private var buttonLogin: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,21 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editTextLogin = findViewById<EditText>(R.id.editTextLogin)
+        editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+        buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin?.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        val login = editTextLogin?.text.toString()
+        val password = editTextPassword?.text.toString()
+
+        println("Login: $login")
+        println("Password: $password")
     }
 }
