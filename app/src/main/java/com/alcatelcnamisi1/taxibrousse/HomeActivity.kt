6d73b14175c328c3622f1118a29070224e1db270
Robin <robin.bechlem@gmail.com>
2024-05-17 16:57:53 +0200

    ajout de l'ouverture du fragement et de la requête api

--- app/src/main/java/com/alcatelcnamisi1/taxibrousse/HomeActivity.kt
+++ app/src/main/java/com/alcatelcnamisi1/taxibrousse/HomeActivity.kt
@@ -1,12 +1,19 @@
 package com.alcatelcnamisi1.taxibrousse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class HomeActivity : AppCompatActivity() {
+
+    private var buttonCreateCommunity: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,20 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        buttonCreateCommunity = findViewById<Button>(R.id.buttonCreateCommunity)
+
+        buttonCreateCommunity?.setOnClickListener{
+            print("button create Community clicked");
+
+            val fragment = CreateCommunityFragment();
+
+            val fragmentManager: FragmentManager = supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frameLayoutCreateCommunity, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
         }
     }
 }
