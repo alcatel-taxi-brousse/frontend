e66ebc3ca2c0462543a33dad9d7824b40fbf63e4
Alexandre <aofferle@cfai-formation.fr>
2024-11-29 16:12:57 +0100

    Suppression des valeurs dans TaxiBrousse pour ne pas exposer les tokens

--- app/src/main/java/com/alcatelcnamisi1/taxibrousse/TaxiBrousse.kt
+++ app/src/main/java/com/alcatelcnamisi1/taxibrousse/TaxiBrousse.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import com.ale.rainbowsdk.RainbowSdk
+import java.util.Properties
+
 
 class TaxiBrousse : Application() {
 
@@ -13,10 +15,18 @@ class TaxiBrousse : Application() {
         val builder = VmPolicy.Builder()
         StrictMode.setVmPolicy(builder.build())
 
+        val properties = Properties()
+        val assetManager = applicationContext.assets
+        val inputStream = assetManager.open("settings.properties")
+        properties.load(inputStream)
+        val app_id = properties.getProperty("app.id")
+        val app_secret = properties.getProperty("app.secret")
+        inputStream.close()
+
         RainbowSdk().initialize(
             applicationContext = this,
-            applicationId = "9527e050808c11efa6661b0bb9c90370",
-            applicationSecret = "UDZBdWsdk6iM4Ke18dzQ8WLbgx2NCg4aGjAXuSfQikcmeNnQj6AvNUJjrc6o1yWP"
+            applicationId = app_id,
+            applicationSecret = app_secret
         )
     }
 }
